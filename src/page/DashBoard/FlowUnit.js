3dd13f84acfa21f3879fea4a765c384f70f87c35
fwchen <crystal.tyan@gmail.com>
2018-01-24 21:32:56 +0800

    配置了一些 eslint 和 flow

--- src/page/DashBoard/FlowUnit.js
+++ src/page/DashBoard/FlowUnit.js
@@ -1,10 +1,12 @@
 // @flow
 import React, { Component } from 'react';
+import R from 'ramda';
 
 export default class FlowUnit extends Component<{
   flowState: FlowState
 }> {
   render() {
+    console.log(1);
     const { flowState } = this.props;
     const className = flowState.status.toLowerCase();
     return <div className={`flow-unit ${className}`}>{flowState.name}</div>;
--- src/page/DashBoard/Status.js
+++ src/page/DashBoard/Status.js
@@ -1,20 +1,19 @@
+// @flow
 import React, { Component } from 'react';
 import { Icon } from 'antd';
 import PropTypes from 'prop-types';
 import R from 'ramda';
 
-export default class Status extends Component {
-  static propTypes = {
-    status: PropTypes.object.isRequired
-  };
-
+export default class Status extends Component<{
+  status: any
+}> {
   render() {
     const { status } = this.props;
     let statusDom;
     if (status.isWaitting) {
       statusDom = <Icon type="clock-circle" />;
     } else if (status.isRunning) {
-      statusDom = <Icon type="loading" />
+      statusDom = <Icon type="loading" />;
     } else if (status.isSuccess) {
       statusDom = <Icon type="check-circle" />;
     } else {
