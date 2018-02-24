bb982e55fd1a7ba14179a5b5d31e369861ec529a
fwchen <crystal.tyan@gmail.com>
2018-02-24 09:46:09 +0800

    update

--- src/reducer/report.reducer.js
+++ src/reducer/report.reducer.js
@@ -25,7 +25,6 @@ export function report(state: Map<string, *> = Map({}), action: FSAction) {
 
     case Actions.WS_GET_PROEJCT_COMMIT_MESSAGE.SUCCESS: {
       return state.updateIn([action.meta.projectName], (list: List<ProjectReport>) => {
-        console.log(list, action);
         return list.update(
           list.findKey((report: Map<ProjectReport>) => report.get('id') === action.meta.reportId),
           report => report.update('commitMessage', () => action.payload)
