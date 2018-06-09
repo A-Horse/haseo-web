e5a8d749d997a0689e8017a57504ee3e3fb882ae
fwchen <crystal.tyan@gmail.com>
2018-06-09 14:11:15 +0800

    update

--- src/page/Header/Header.js
+++ src/page/Header/Header.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import { Logo } from '../../component/Logo/Logo';
 import { Divider, Button } from 'antd';
+import { Link } from 'react-router-dom';
 
 import './Header.less';
 
@@ -12,10 +13,12 @@ export class AppHeader extends Component<{
   render() {
     return (
       <header className="app-header">
-        <div className="header-ban">
-          <Logo />
-          <span>Haseo</span>
-        </div>
+        <Link to="/">
+          <div className="header-ban">
+            <Logo />
+            <span>Haseo</span>
+          </div>
+        </Link>
 
         {this.props.user && (
           <div className="header-user">
