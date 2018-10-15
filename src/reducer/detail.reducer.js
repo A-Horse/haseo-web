b3f124b07431b4a9359cb5f67c3cfa6391a5fbfe
fwchen <crystal.tyan@gmail.com>
2018-10-16 00:08:37 +0800

    transformFlowDescriptionMap for report flows

--- src/reducer/detail.reducer.js
+++ src/reducer/detail.reducer.js
@@ -1,11 +1,16 @@
 // @flow
 import { Map, List, fromJS } from 'immutable';
 import Actions from '../action/actions';
+import { transformFlowDescriptionMap } from '../util/project-helpers';
 
 export function detail(state: Map<string, *> = Map({ reports: List() }), action: FSAction) {
   switch (action.type) {
     case Actions.WS_GET_PROJECT_REPORT_HISTORY.SUCCESS: {
-      const reports: ProjectReport = action.payload;
+      const reports: ProjectReport = action.payload.map(report => ({
+        ...report,
+        flows: (report.flows || []).map(flow => transformFlowDescriptionMap(flow))
+      }));
+
       return state.set('reports', fromJS(reports));
     }
 
