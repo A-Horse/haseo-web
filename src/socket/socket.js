b335094fa2ba35b10e63d8f59eb87192d4f03d23
fwchen <crystal.tyan@gmail.com>
2018-11-16 11:56:27 +0800

    auto switch ws or wss

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -3,5 +3,11 @@ import { Observable } from 'rxjs/Observable';
 import { WebSocketSubject } from 'rxjs/observable/dom/WebSocketSubject';
 
 export function createSocket$(): WebSocketSubject<FSAction> {
-  return Observable.webSocket(`ws://${location.host}/ws`);
+  let websocketProtocol;
+  if (location.protocol != 'https:') {
+    websocketProtocol = 'ws';
+  } else {
+    websocketProtocol = 'wss';
+  }
+  return Observable.webSocket(`${websocketProtocol}://${location.host}/ws`);
 }
